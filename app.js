9abac1bf9ee448d0e5ae9b0965dfa2d0f1157fef
Nick <nick@Nicks-MacBook-Pro.local>
2019-09-02 16:19:27 -0400

    First commit. Basic liquid animation effect working on images

---
+++ app.js
@@ -0,0 +1,17 @@
+
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "./kaapo.jpg",
+  image2: "./kaapo-shoot.jpg",
+  displacementImage: "./stripes.png"
+});
+
+
+new hoverEffect({
+  parent: document.querySelector(".distortion2"),
+  intensity: 0.2,
+  image1: "./jack.jpg",
+  image2: "./hughes-card.jpg",
+  displacementImage: "./stripes.png"
+});
